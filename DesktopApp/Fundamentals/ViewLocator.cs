facdbc33ad977a57966e6103902f0e080f33c8e2
somni <me@somni.one>
2026-05-25 22:28:00 +0900

    Update EditorConfig

--- DesktopApp/Fundamentals/ViewLocator.cs
+++ DesktopApp/Fundamentals/ViewLocator.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using Somni.Maki.ViewModels;
 
 namespace Somni.Maki.Fundamentals {
   /// <summary>
@@ -13,14 +12,14 @@ namespace Somni.Maki.Fundamentals {
     Url = "https://docs.avaloniaui.net/docs/concepts/view-locator")]
   public class ViewLocator : IDataTemplate {
     public Control? Build(object? param) {
-      if (param is null) {
+      if(param is null) {
         return null;
       }
 
       string name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
       Type? type = Type.GetType(name);
 
-      if (type != null) {
+      if(type != null) {
         return (Control)Activator.CreateInstance(type)!;
       }
 
