6957cbef7ec3ef0c7c59f027aa47f91cd98f155c
somni <me@somni.one>
2026-05-25 22:04:34 +0900

    Initial commit with Avalonia UI-based initial Desktop App project

---
+++ DesktopApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿using Somni.Maki.Fundamentals;
+
+namespace Somni.Maki.ViewModels {
+  public class MainWindowViewModel : ViewModelBase {
+    public string Greeting { get; } = "Welcome to Avalonia!";
+  }
+}
